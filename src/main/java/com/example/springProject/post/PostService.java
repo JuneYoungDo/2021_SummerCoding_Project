0a23defe7425402deccaee2a90497a45a4adf84b
JuneYoungDo <ehwnsdud1004@naver.com>
2021-08-11 15:10:51 +0900

    [BLOG-2]Post_CRUD_PostService

--- src/main/java/com/example/springProject/post/PostService.java
+++ src/main/java/com/example/springProject/post/PostService.java
@@ -2,6 +2,27 @@ package com.example.springProject.post;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post findById(Long postId) {
+        return postRepository.findById(postId);
+    }
+    public Post findByTitle(String postTitle) {
+        return postRepository.findByTitle(postTitle);
+    }
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+    public void save(Post post) {
+        postRepository.save(post);
+    }
+
 }
