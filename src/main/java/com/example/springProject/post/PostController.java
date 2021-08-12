171675384ce2dc1e4e8b5e0a00ff5550a8523711
JuneYoungDo <ehwnsdud1004@naver.com>
2021-08-12 17:04:10 +0900

    [BLOG-2]Post_CRUD_PostController

--- src/main/java/com/example/springProject/post/PostController.java
+++ src/main/java/com/example/springProject/post/PostController.java
@@ -1,9 +1,12 @@
 package com.example.springProject.post;
 
+import com.example.springProject.post.form.PostForm;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -42,12 +45,54 @@ public class PostController {
     }
 
     /**
+     * 게시글 생성 폼 연결
+     * GET /posts/new-post
+     */
+    @GetMapping("/posts/new-post")
+    public String newPost(Model model) {
+        model.addAttribute("postForm",new PostForm());
+        return "/posts/new-post";
+    }
+
+    /**
      * 게시글 생성
      * POST /posts
      */
+    @PostMapping("/posts/new-post")
+    public String createPost(PostForm postForm) {
+        Post post = new Post(
+                postForm.getId(),
+                postForm.getTitle(),
+                postForm.getDescription()
+        );
+        postService.save(post);
+        return "redirect:/posts";
+    }
 
     /**
      * 게시글 수정
      * POST /posts/{post-id}
      */
+    @GetMapping("/posts/edit-post/{postId}")
+    public String editPost(@PathVariable Long postId, Model model) {
+        Post post = postService.findById(postId);
+        model.addAttribute("post",post);
+        model.addAttribute("postForm",new PostForm(
+                post.getId(),
+                post.getTitle(),
+                post.getDescription()
+        ));
+
+        return "posts/edit-post";
+    }
+
+    @PostMapping("posts/edit-post/{postId}")
+    public String editPost(@PathVariable Long postId,PostForm postForm) {
+        Post post = postService.findById(postId);
+        post.setId(postForm.getId());
+        post.setTitle(postForm.getTitle());
+        post.setDescription(postForm.getDescription());
+
+        return "redirect:/posts";
+    }
 }
